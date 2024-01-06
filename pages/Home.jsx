256c2a24b9ab9e1fd358e24a1fe11a463848c70a
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-01-06 13:27:22 +0530

    change

---
+++ pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../shared/Footer";
+import Header from "../shared/Header";
+
+const Home = () => {
+  return (
+    <>
+    <button className="btn btn-active btn-primary">Primary</button>
+<button className="btn btn-active btn-secondary">Secondary</button>
+      <Header />
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
